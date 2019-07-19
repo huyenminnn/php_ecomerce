ea37428cc413d631e0337f76b64a087cae996485
huyenminnn <huyenttm.13@gmail.com>
2019-07-19 13:47:47 +0700

    pages for user

---
+++ resources/lang/en/shopping.php
@@ -0,0 +1,72 @@
+<?php
+
+return [
+    'layout' => [
+        'freeship' => 'Free shipping for standard order over $50!',
+        'home' => 'Home',
+        'shop' => 'Shop',
+        'bestSeller' => 'Best Seller',
+        'blog' => 'Blog',
+        'about' => 'About',
+        'contact' => 'Contact',
+        'cart' => 'View Cart',
+        'checkout' => 'Check Out',
+        'total' => 'Total: ',
+        'getInTouch' => 'GET IN TOUCH',
+        'address' => 'Any questions? Let us know in store at 8th floor, 601 Giai Phong St, Hoang Mai, Hanoi  or call us on (+84) 96 716 6879',
+        'categories' => 'Categories',
+        'link' => 'Links',
+        'search' => 'Search',
+        'aboutUs' => 'About Us',
+        'contactUs' => 'Contact Us',
+        'return' => 'Returns',
+        'help' => 'Help',
+        'email' => 'Email',
+        'shipping' => 'Shipping',
+        'FAQs' => 'FAQs',
+        'trackOrder' => 'Track Order',
+    ],
+
+    'home' => [
+        'newArr' => 'New arrivals',
+        'shopNow' => 'Shop Now',
+        'featuredProducts' => 'Featured Products',
+        'addToCart' => 'Add to Cart',
+        'size' => 'Size',
+        'color' => 'Color',
+        'code' => 'Code: ',
+        'categories' => 'Categories: ',
+        'description' => 'Description',
+        'relatedP' => 'Related Products',
+    ],
+
+    'cart' => [
+        'cart' => 'Cart',
+        'product' => 'Product',
+        'price' => 'Price',
+        'quantity' => 'Quantity',
+        'total' => 'Total',
+        'coupon' => 'Apply coupon',
+        'updateCart' => 'Update Cart',
+        'cartTotal' => 'Cart Totals',
+        'subtotal' => 'Subtotal: ',
+        'shipping' => 'Shipping: ',
+        'feeShip' => 'Calculate Shipping',
+        'updateTotal' => 'Update Totals',
+        'checkout' => 'Proceed to Checkout',
+    ],
+
+    'suggest' => [
+        'product' => 'Suggest Product',
+        'sendMess' => 'Send us your message',
+        'send' => 'Send',
+    ],
+
+    'home' => [
+        'home' => 'Home',
+        'login' => 'Login',
+        'register' => 'Register',
+        'dashboard' => 'Dashboard',
+        'logged' => 'You are logged in!',
+    ],
+];
